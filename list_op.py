0dbaefeec9aa6c5938da2ebe6044b1e14a4f0917
clemto <clem.larmi@gmail.com>
2025-05-14 14:49:10 +0200

    add lit op

--- list_op.py
+++ list_op.py
@@ -1,3 +1,6 @@
+from itertools import batched
+
+
 def sous_ensembles_k(tab, k):
     return [tab[i:i+k] for i in range(0, len(tab), k)]
 
@@ -6,3 +9,6 @@ l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 k = 3
 result = sous_ensembles_k(l, k)
 print(result)
+
+r_batch = [list(i) for i in list(batched([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3))]
+print(r_batch)
