a9a1a898fe1429b2039c8202c10a431806270324
clemto <clem.larmi@gmail.com>
2025-05-14 13:03:30 +0200

    change most k

--- most_k_list.py
+++ most_k_list.py
@@ -1,31 +1,23 @@
-def at_most_k_distinct(arr, k):
-    count = {}  # dictionnaire classique
-    left = 0
-    result = 0
-
-    for i in range(len(arr)):
-        if arr[i] not in count:
-            count[arr[i]] = 0
-        count[arr[i]] += 1
-
-        if count[arr[i]] == 1:
-            k -= 1
-
-        while k < 0:
-            count[arr[left]] -= 1
-            if count[arr[left]] == 0:
-                del count[arr[left]]
-                k += 1
-            left += 1
-
-        result += i - left + 1
-
-    return result
-
 def subarrays_with_at_least_k_distinct(arr, k):
-    if k == 0:
-        return (len(arr) * (len(arr) + 1)) // 2
-    return at_most_k_distinct(arr, len(arr)) - at_most_k_distinct(arr, k - 1)
+    def at_most_k(arr, k):
+        count = 0
+        left = 0
+        freq = {}
+        for right in range(len(arr)):
+            if arr[right] not in freq:
+                freq[arr[right]] = 0
+            freq[arr[right]] += 1
+
+            while len(freq) > k:
+                freq[arr[left]] -= 1
+                if freq[arr[left]] == 0:
+                    del freq[arr[left]]
+                left += 1
+
+            count += right - left + 1
+        return count
+
+    return at_most_k(arr, k) - at_most_k(arr, k - 1)
 
 # Exemple
 arr = [2, 1, 2, 1, 6]
